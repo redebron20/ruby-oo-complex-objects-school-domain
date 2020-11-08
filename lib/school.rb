87735c542e7768992f7003bf150a862db4245631
redebron20 <redebron20@gmail.com>
2020-11-08 06:00:12 -0500

    commit tree 0f49061b9d8fac5a8629b8d1442a99ea02b83bf5

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,33 @@
 # code here!
+require 'pry'
+
+class School
+    attr_accessor
+    attr_writer :add_student
+
+    def initialize(roster)
+        @roster = {}
+    end
+
+    def roster
+        @roster
+    end
+
+
+    def add_student(name, grade)
+        @roster[grade] ||= []
+        @roster[grade] << name
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+        @roster.each do |grade, students|
+            students.sort!
+        end
+    end
+    
+
+end
